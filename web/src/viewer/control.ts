61a63444414dd0fcb93ca4b7768b3adaa3a63775
ZZYSonny <zhaozy5520@outlook.com>
2022-07-10 18:44:00 +0100

    add n k key

--- web/src/viewer/control.ts
+++ web/src/viewer/control.ts
@@ -1,3 +1,4 @@
+import { getCurVPBox } from "src/helper/viewport";
 import { IBackend, IDocInteract, PDFOutlineObject } from "../pdf/interface";
 import { ILayout } from "../pdf/layout";
 import { DocViewer } from "./viewer";
@@ -36,5 +37,15 @@ export class DocViewerController extends DocViewer{
     menu.id = "MenuOutline"
     menu.appendChild(outlinehtml)
     document.body.appendChild(menu);
+
+    document.addEventListener('keydown', ev => {
+      const curPageNum = layout.getPageRange(getCurVPBox())[0]-1;
+      if(ev.key=='n'){
+        layout.scrollTo(Math.min(curPageNum+1, doc.pageinfo.doc_pages - 1),0,0);
+      } else if(ev.key=='k'){
+        layout.scrollTo(Math.max(curPageNum-1, 0),0,0);
+      }
+    })
+  
   }
 }
